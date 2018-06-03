d5c32d77ee324d143399ed6d722fcecd495b86b7
bogac <bogac@DESKTOP-05IMPEI>
2018-06-03 16:57:20 +0300

    Calculator'a ManagedBean eklendi!

---
+++ src/main/java/com/bilisimegitim/calculatorjsf/mb/Calculator.java
@@ -0,0 +1,17 @@
+
+package com.bilisimegitim.calculatorjsf.mb;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+@Named(value = "calculator")
+@RequestScoped
+public class Calculator {
+
+    /**
+     * Creates a new instance of Calculator
+     */
+    public Calculator() {
+    }
+    
+}
